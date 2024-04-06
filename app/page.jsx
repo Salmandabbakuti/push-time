45882c9d646eeedbae245d4fe1f10ca6997aee3f
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-04-06 11:23:17 +0530

    Develop (#6)
    * updated: few fixes
    
    * fix: initialization error when video call is ended or denied
    
    * added .env example, provider with rpc url
    
    * validation feedback
    
    * added: remote stream audio, video feedbacks
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * added: hero section

--- app/page.jsx
+++ app/page.jsx
@@ -15,12 +15,13 @@ import { IoMdMic, IoMdMicOff, IoMdVideocam } from "react-icons/io";
 import { IoVideocamOff } from "react-icons/io5";
 import { MdCallEnd } from "react-icons/md";
 import { IoCall } from "react-icons/io5";
-import { useSigner } from "@thirdweb-dev/react";
+import { useSigner, useAddress } from "@thirdweb-dev/react";
 import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
 import { getDefaultProvider } from "@ethersproject/providers";
 import { isAddress } from "@ethersproject/address";
+import styles from "./page.module.css";
 
-const provider = getDefaultProvider();
+const provider = getDefaultProvider(process.env.NEXT_PUBLIC_MAINNET_RPC_URL);
 
 const addressToEns = async (address) => {
   try {
@@ -46,11 +47,14 @@ export default function Home() {
   const [incomingCaller, setIncomingCaller] = useState(null);
   const [loading, setLoading] = useState({});
   const pushUserVideo = useRef(null);
+
   const signer = useSigner();
+  const account = useAddress();
 
   const incomingStatus = data?.incoming[0]?.status;
 
   const init = async () => {
+    console.log("init is calling", signer, data);
     if (!signer) return message.error("Please connect your wallet");
     const pushUser = await PushAPI.initialize(signer, {
       env: CONSTANTS.ENV.STAGING
@@ -68,6 +72,7 @@ export default function Home() {
       console.log(data);
       if (data.event === CONSTANTS.VIDEO.EVENT.REQUEST) {
         console.log("Call request received");
+        console.log("data on request", data);
         const callerEnsish = await addressToEns(data?.peerInfo?.address);
         console.log("Caller ENSish", callerEnsish);
         setIncomingCaller(callerEnsish || data?.peerInfo?.address);
@@ -76,21 +81,25 @@ export default function Home() {
       if (data.event === CONSTANTS.VIDEO.EVENT.APPROVE) {
         console.log("Call approved");
         message.success("Call connected");
+        console.log("data on approve", data);
       }
 
       if (data.event === CONSTANTS.VIDEO.EVENT.DENY) {
         console.log("Call denied");
         message.error("Call denied");
+        console.log("data on deny", data);
       }
 
       if (data.event === CONSTANTS.VIDEO.EVENT.CONNECT) {
         console.log("Call connected");
         message.success("Call connected");
+        console.log("data on connect2", data);
       }
 
       if (data.event === CONSTANTS.VIDEO.EVENT.DISCONNECT) {
         console.log("Call disconnected");
         message.success("Call disconnected");
+        console.log("data on disconnect2", data);
       }
     });
 
@@ -98,8 +107,11 @@ export default function Home() {
   };
 
   useEffect(() => {
-    console.log(data);
-    if (signer || incomingStatus === CONSTANTS.VIDEO.STATUS.UNINITIALIZED) {
+    console.log(signer, data);
+    if (!signer) return;
+    // initialize the video stream when the video call is not initialized, call ended or denied.
+    // only call init when signer is available and video call is not initialized or call ended or denied.
+    if (!data || incomingStatus === CONSTANTS.VIDEO.STATUS.UNINITIALIZED) {
       init();
     }
   }, [signer, incomingStatus]);
@@ -186,12 +198,20 @@ export default function Home() {
 
   return (
     <div>
-      <Form
-        onFinish={handleMakeCall}
-        style={{ textAlign: "center", marginTop: "50px" }}
-      >
-        <Row justify="center" style={{ marginTop: "50px" }}>
-          <Col span={8}>
+      {account ? (
+        <>
+          <h2 style={{ textAlign: "center", marginTop: "20px" }}>
+            Ready to make a call?
+          </h2>
+          <Form
+            onFinish={handleMakeCall}
+            style={{
+              textAlign: "center",
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
             <Form.Item
               name="address"
               hasFeedback
@@ -206,130 +226,183 @@ export default function Home() {
                 }
               ]}
             >
-              <Input
-                placeholder="Enter Address or ENS name"
-                onChange={(e) => setRecipientAddress(e.target.value)}
-                required
-                suffix={
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    shape="round"
-                    size="middle"
-                    icon={<IoCall />}
-                    loading={loading?.makeCall}
-                  >
-                    {loading.makeCall ? "Calling..." : "Call"}
-                  </Button>
-                }
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-      <Card>
-        <Row justify="center" style={{ marginTop: "50px" }}>
-          <Col span={8}>
-            <Card title="Your Video" style={{ textAlign: "center" }}>
-              {/* Your video stream here */}
-              <video
-                ref={localVideoRef}
-                autoPlay
-                muted
-                style={{ width: "100%" }}
-              >
-                {/* Add your video stream source here */}
-              </video>
-            </Card>
-          </Col>
-          {incomingStatus === CONSTANTS.VIDEO.STATUS.CONNECTED && (
-            <Col span={8}>
-              <Card title="Remote User's Video" style={{ textAlign: "center" }}>
-                {/* Remote user's video stream here */}
-                <video
-                  ref={remoteVideoRef}
-                  autoPlay
-                  style={{ width: "100%" }}
+              <Space>
+                <Input
+                  size="large"
+                  placeholder="Enter Address or ENS name"
+                  onChange={(e) => setRecipientAddress(e.target.value)}
+                  required
                 />
-              </Card>
-            </Col>
-          )}
-        </Row>
-        {/* actions buttons centered */}
-        <Row
-          justify="center"
-          style={{
-            textAlign: "center",
-            marginTop: "10px"
-          }}
-        >
-          <Col span={8}>
-            <Space>
-              <Button
-                onClick={handleToggleVideo}
-                icon={data?.local?.video ? <IoMdVideocam /> : <IoVideocamOff />}
-              />
-              <Button
-                onClick={handleToggleAudio}
-                icon={data?.local?.audio ? <IoMdMic /> : <IoMdMicOff />}
-              />
-              {/* show end call button only when remote incomingStatus */}
-              {incomingStatus === CONSTANTS.VIDEO.STATUS.CONNECTED && (
                 <Button
-                  onClick={handleEndCall}
-                  icon={<MdCallEnd />}
-                  danger
                   type="primary"
-                />
+                  htmlType="submit"
+                  shape="round"
+                  size="large"
+                  icon={<IoCall />}
+                  loading={loading?.makeCall}
+                >
+                  {loading.makeCall ? "Calling..." : "Call"}
+                </Button>
+              </Space>
+            </Form.Item>
+          </Form>
+          <Card>
+            <Row justify="center" style={{ marginTop: "50px" }}>
+              <Col span={8}>
+                <Card title="Your Video" style={{ textAlign: "center" }}>
+                  {/* Your video stream here */}
+                  <video
+                    ref={localVideoRef}
+                    autoPlay
+                    muted
+                    style={{ width: "100%" }}
+                  />
+                </Card>
+              </Col>
+              {incomingStatus === CONSTANTS.VIDEO.STATUS.CONNECTED && (
+                <Col span={8}>
+                  <Card
+                    title="Remote User's Video"
+                    style={{ textAlign: "center" }}
+                  >
+                    {/* Remote user's video stream here */}
+                    <video
+                      ref={remoteVideoRef}
+                      autoPlay
+                      style={{ width: "100%" }}
+                    />
+                    {/* add video, audio icons based on remote user's settings */}
+                    <Space>
+                      <Button
+                        type="text"
+                        style={{
+                          color: data?.incoming[0]?.video ? "green" : "red"
+                        }}
+                        icon={
+                          data?.incoming[0]?.video ? (
+                            <IoMdVideocam />
+                          ) : (
+                            <IoVideocamOff />
+                          )
+                        }
+                      />
+                      <Button
+                        type="text"
+                        style={{
+                          color: data?.incoming[0]?.audio ? "green" : "red"
+                        }}
+                        icon={
+                          data?.incoming[0]?.audio ? (
+                            <IoMdMic />
+                          ) : (
+                            <IoMdMicOff />
+                          )
+                        }
+                      />
+                    </Space>
+                  </Card>
+                </Col>
               )}
-            </Space>
-          </Col>
-        </Row>
-      </Card>
+            </Row>
+            {/* actions buttons centered */}
+            <Row
+              justify="center"
+              style={{
+                textAlign: "center",
+                marginTop: "10px"
+              }}
+            >
+              <Col span={8}>
+                <Space>
+                  <Button
+                    style={{ color: data?.local?.video ? "green" : "red" }}
+                    onClick={handleToggleVideo}
+                    icon={
+                      data?.local?.video ? <IoMdVideocam /> : <IoVideocamOff />
+                    }
+                  />
+                  <Button
+                    style={{ color: data?.local?.audio ? "green" : "red" }}
+                    onClick={handleToggleAudio}
+                    icon={data?.local?.audio ? <IoMdMic /> : <IoMdMicOff />}
+                  />
+                  {/* show end call button only when remote incomingStatus */}
+                  {incomingStatus === CONSTANTS.VIDEO.STATUS.CONNECTED && (
+                    <Button
+                      onClick={handleEndCall}
+                      icon={<MdCallEnd />}
+                      danger
+                      type="primary"
+                    />
+                  )}
+                </Space>
+              </Col>
+            </Row>
+          </Card>
 
-      <Modal
-        title="Incoming Call"
-        open={
-          incomingStatus === CONSTANTS.VIDEO.STATUS.RECEIVED && incomingCaller
-        }
-        footer={[
-          <Button
-            style={{
-              color: "white",
-              backgroundColor: "#00FF00",
-              borderColor: "#00FF00"
-            }}
-            key="accept"
-            type="primary"
-            loading={loading?.acceptIncomingCall}
-            icon={<IoCall />}
-            onClick={handleAcceptIncomingCall}
+          <Modal
+            title="Incoming Call"
+            open={
+              incomingStatus === CONSTANTS.VIDEO.STATUS.RECEIVED &&
+              incomingCaller
+            }
+            footer={[
+              <Button
+                style={{
+                  color: "white",
+                  backgroundColor: "#00FF00",
+                  borderColor: "#00FF00"
+                }}
+                key="accept"
+                type="primary"
+                loading={loading?.acceptIncomingCall}
+                icon={<IoCall />}
+                onClick={handleAcceptIncomingCall}
+              >
+                Accept
+              </Button>,
+              <Button
+                style={{
+                  color: "white",
+                  backgroundColor: "#FF0000",
+                  borderColor: "#FF0000"
+                }}
+                key="reject"
+                icon={<MdCallEnd />}
+                danger
+                loading={loading.rejectIncomingCall}
+                onClick={handleRejectIncomingCall}
+              >
+                Reject
+              </Button>
+            ]}
           >
-            Accept
-          </Button>,
-          <Button
-            style={{
-              color: "white",
-              backgroundColor: "#FF0000",
-              borderColor: "#FF0000"
-            }}
-            key="reject"
-            icon={<MdCallEnd />}
-            danger
-            loading={loading.rejectIncomingCall}
-            onClick={handleRejectIncomingCall}
-          >
-            Reject
-          </Button>
-        ]}
-      >
-        {incomingStatus === CONSTANTS.VIDEO.STATUS.RECEIVED &&
-          incomingCaller && (
-            <div>
-              <p>{`${incomingCaller} is calling.`}</p>
-            </div>
-          )}
-      </Modal>
+            <p>{`${incomingCaller} is calling..`}</p>
+          </Modal>
+        </>
+      ) : (
+        // add landing page content here
+        <div className={styles.heroSection}>
+          <h1>
+            Welcome to{" "}
+            <p
+              style={{
+                color: "blue",
+                display: "inline",
+                fontWeight: "bold",
+                fontSize: "1.5em"
+              }}
+            >
+              PushTime
+            </p>
+          </h1>
+          <h2>
+            PushTime is a decentralized video calling application built with
+            Push Protocol. A place to connect with your Web3 friends and family.
+          </h2>
+          <h2>Please connect your wallet to get started!</h2>
+        </div>
+      )}
     </div>
   );
 }
